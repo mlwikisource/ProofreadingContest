f033ae9b176318a0047742cfd21adc798c0f26b9
Balasankar C <c.balasankar@gmail.com>
2013-12-30 22:09:44 +0530

    Tabulating and Linking

--- Proofreading_Contest_Count.py
+++ Proofreading_Contest_Count.py
@@ -13,8 +13,8 @@ category = str(sys.argv[1]).decode("UTF-8")
 wikipedia.setLogfileStatus(True)
 wikiSite = wikipedia.Site(code=siteLangCode, fam=siteFamily)
 cat = catlib.Category(wikiSite,"Category:"+category)
-fp = open("count.csv","w")
-list = cat.articlesList(recurse=False)
+fp = open("a.csv","w")
+list = cat.articlesList(recurse=True)
 out = {}
 for i in list:
     print i.title()
@@ -34,11 +34,23 @@ for i in valueunique:
     countlist[i]=count
 sortname = sorted(countlist.items(), key=lambda x:x[1])
 stat={}
+namelist=[]
+countlist=[]
 for i in sortname:
     splited=str(i).split(',')
     name=splited[0][3:-1]
-    count=splited[1][:-1]
-    stat[name]=count
-for key,value in stat.items():
-    fp.write(key.encode("UTF-8") + "," + str(value) + "\n")
+    count=splited[1][1:-1]
+ #   namelist.append(name)
+ #   countlist.append(count)
+    stat[name]=int(count)
+#bothlist=zip(countlist,namelist)
+#bothlist.sort()
+#print bothlist
+out="<table>"
+for key,value in sorted(stat.iteritems(), key=lambda (v,k): (k,v),reverse=True):
+    out=out+"<tr><td>[[User:"+key.encode("UTF-8") + "]]</td><td>[[Special:Contributions/" + key.encode("UTF-8")+"|"+ str(value) + "]]</td></tr>"
+out=out+"</table>"
+resultPage="User:Balasankarc\count"
+myResultPage=wikipedia.Page(site=wikiSite,title=resultPage)
+myResultPage.put(out,comment=ur"Calculating Count")
 fp.close()
