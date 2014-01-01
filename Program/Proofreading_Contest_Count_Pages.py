acb4e31b65413d3faab859c0ef5cf7276f222744
Balasankar C <c.balasankar@gmail.com>
2014-01-01 10:49:15 +0530

    Modifications

--- Program/Proofreading_Contest_Count_Pages.py
+++ Program/Proofreading_Contest_Count_Pages.py
@@ -13,9 +13,10 @@ category = "WSDCPages"
 wikipedia.setLogfileStatus(True)
 wikiSite = wikipedia.Site(code=siteLangCode, fam=siteFamily)
 cat = catlib.Category(wikiSite,"Category:"+category)
-fp = open("/home/balasankarc/git/ProofreadingContest/Statistics/index.html","w")
+fp = open("/var/www/clients/client23/web27/web/ProofreadingContest/index.html","w")
 list = cat.articlesList(recurse=False)
 noofarticles=len(list)
+per = noofarticles*100/1496
 stat={}
 for i in list:
     print i.title()
@@ -26,7 +27,7 @@ for i in list:
         stat[creator[0]] = 1
 wikipedia.stopme()
 print stat
-out='<link href="css/bootstrap.css" rel="stylesheet" /><body style="margin-left:10%;margin-right:10%;">    <h1 style="text-align:center;">        <img src="images/Wikisource-logo.png" width="100px" height="100px" /><br /><big>വിക്കിഗ്രന്ഥശാല    </big>   <br /> മത്സരത്തിന്റെ കണക്കുകൾ  </h1> <div class="progress  progress-striped active">  <div class="progress-bar progress-bar-danger"  role="progressbar" aria-valuenow="'+str(noofarticles)+'" aria-valuemin="0" aria-valuemax="100" style="width: '+str(noofarticles)+'%">    <span class="sr-only">45% Complete</span>  </div></div>'
+out='<link href="css/bootstrap.css" rel="stylesheet" /><body style="margin-left:10%;margin-right:10%;">    <h1 style="text-align:center;">        <img src="images/Wikisource-logo.png" width="100px" height="100px" /><br /><big>വിക്കിഗ്രന്ഥശാല    </big>   <br /> മത്സരത്തിന്റെ കണക്കുകൾ  </h1> <div class="progress  progress-striped active">  <div class="progress-bar progress-bar-danger"  role="progressbar" aria-valuenow="'+str(noofarticles)+'" aria-valuemin="0" aria-valuemax="100" style="width: '+str(per)+'%">    <span class="sr-only">45% Complete</span>  </div></div>'
 out=out+'<table class="table table-bordered">'
 for key,value in sorted(stat.iteritems(), key=lambda (v,k): (k,v),reverse=True):
     out=out+"<tr><td><a href='http://ml.wikisource.org/wiki/User:"+key.encode("UTF-8") + "'>"+key.encode("UTF-8")+"</a></td><td><a href='http://ml.wikisource.org/wiki/Special:Contributions/" + key.encode("UTF-8")+"'>"+ str(value) + "</a></td></tr>"
